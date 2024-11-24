d82d7fa82fe2ebc539ab53660433085b7e3e5c7a
Yahiasky <129469342+Yahiasky@users.noreply.github.com>
2024-11-24 18:43:00 +0100

    yahia_test (just add comment)

--- app/src/main/java/com/app/leaflet/MainActivity.kt
+++ app/src/main/java/com/app/leaflet/MainActivity.kt
@@ -8,7 +8,7 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-
+//test_yahia
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
