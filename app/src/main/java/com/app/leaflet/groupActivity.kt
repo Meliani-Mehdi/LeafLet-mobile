4c7c730abac63fd7152113081e57736c155dc9b8
mokhefu <126604497+mokhefu@users.noreply.github.com>
2024-11-30 11:39:11 +0100

    added group activity

---
+++ app/src/main/java/com/app/leaflet/groupActivity.kt
@@ -0,0 +1,29 @@
+package com.app.leaflet
+
+import android.os.Bundle
+import android.view.View
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class groupActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_group)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+
+    fun go_back(v: View){
+        finish()
+    }
+    fun confirm(v:View){
+
+    }
+
+}
