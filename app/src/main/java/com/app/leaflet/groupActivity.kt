ab2eb4aa3ae2ce2015ce3148d5332b952483fc36
Meliani Mehdi <mehdi.hatachi@gmail.com>
2024-12-06 18:54:47 +0100

    did some changes to group activity.

--- app/src/main/java/com/app/leaflet/groupActivity.kt
+++ app/src/main/java/com/app/leaflet/groupActivity.kt
@@ -2,12 +2,20 @@ package com.app.leaflet
 
 import android.os.Bundle
 import android.view.View
+import android.widget.ArrayAdapter
+import android.widget.EditText
+import android.widget.Spinner
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class groupActivity : AppCompatActivity() {
+
+    private lateinit var groupName: EditText
+    private lateinit var groupType: Spinner
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -17,11 +25,38 @@ class groupActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        groupName = findViewById<EditText>(R.id.groupName)
+        groupType = findViewById<Spinner>(R.id.groupType)
+
+        val group_types = listOf("TD", "TP")
+
+        val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, group_types)
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+
+        groupType.adapter = adapter
+
+
     }
 
     fun go_back(v: View){
         finish()
     }
+
+    fun checkFields(): Boolean {
+        if(groupName.text.trim().isEmpty()){
+            groupName.error = "This field is required"
+            return false
+        }
+
+        if(groupType.selectedItem == null || groupType.selectedItem.toString().trim().isEmpty()) {
+            Toast.makeText(this, "Please select a valid Type", Toast.LENGTH_SHORT).show()
+            return false
+        }
+
+        return true
+    }
+
     fun confirm(v:View){
 
     }
